1c865c8f42039997ab80efb5f333f1b122f17cf5
Mirna Dzamonja <64315491+mirnazemlji@users.noreply.github.com>
2026-06-15 11:38:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,75 +1,44 @@
 $(document).ready(function() {
 
-    // Smooth scroll and navigation
     $('.scrolly').on('click', function(e) {
         e.preventDefault();
         
         const target = $(this).data('page') || $(this).attr('href');
-        const pageType = $(this).data('page-type');
         
-        // Highlight active nav
         $('.tm-main-nav li a').removeClass('active');
         $(this).addClass('active');
         
-        // Load the corresponding section
-        loadSection(target, pageType);
+        loadSection(target);
     });
 
-    // Load the default section (Introduction) on page load
-    loadSection('#tm-section-1');
+    loadSection('#tm-section-1');   // default
 
-    // Initialize carousel if needed
-    function initCarousel() {
-        $('.tm-img-slider').slick({
-            dots: true,
-            infinite: true,
-            speed: 800,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 4000,
-        });
-    }
-
-    function loadSection(sectionId, pageType = null) {
+    function loadSection(sectionId) {
         $('#main-content').html('<div class="loading">Loading...</div>');
 
-        // For now, we'll load from separate HTML files.
-        // Later we can improve this.
         let fileName = '';
 
         switch(sectionId) {
-            case '#tm-section-1':
-                fileName = 'sections/intro.html';
-                break;
-            case '#tm-section-2':
-                fileName = 'sections/highlights.html';
-                break;
-            case '#tm-section-3':
-                fileName = 'sections/publications.html';
-                break;
-            case '#tm-section-4':
-                fileName = 'sections/ethics.html';
-                break;
-            case '#tm-section-5':
-                fileName = 'sections/chat.html';
-                break;
-            case '#tm-section-15':   // Cours E(n)t(r)é en Maths
-                fileName = 'sections/cours.html';
-                break;
-            default:
-                fileName = 'sections/intro.html';
+            case '#tm-section-1': fileName = 'sections/intro.html'; break;
+            case '#tm-section-2': fileName = 'sections/highlights.html'; break;
+            case '#tm-section-3': fileName = 'sections/publications.html'; break;
+            case '#tm-section-4': fileName = 'sections/ethics.html'; break;
+            case '#tm-section-5': fileName = 'sections/chat.html'; break;
+            default: fileName = 'sections/intro.html';
         }
 
-        $.get(fileName, function(data) {
-            $('#main-content').html(data);
-            
-            if (pageType === "carousel") {
-                initCarousel();
-            }
-        }).fail(function() {
-            $('#main-content').html('<p>Error loading content. Please refresh the page.</p>');
-        });
+        $.get(fileName)
+            .done(function(data) {
+                $('#main-content').html(data);
+            })
+            .fail(function() {
+                $('#main-content').html(`
+                    <p style="color:red; padding:30px;">
+                        Error loading ${fileName}<br><br>
+                        Check that the file exists in the <strong>sections/</strong> folder.
+                    </p>
+                `);
+            });
     }
 
 });
